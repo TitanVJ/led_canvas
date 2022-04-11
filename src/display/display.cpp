532b2d4d138373322cbe15a33359a16c968e46ea
vj <vbakhshi@sfu.ca>
2022-04-10 21:30:03 -0700

    done display code, frame building code, and have an executable that will display the sfu+leaf logo

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -1,4 +1,5 @@
 #include "display.h"
+#include "frame.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -6,9 +7,11 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <algorithm>
+#include <cassert>
 
 using LED::Display;
-
+using LED::Frame;
 
 const std::string GPIO_BASE_PATH{"/sys/class/gpio/"};
 constexpr const long DELAY_IN_US = 5;
@@ -30,30 +33,10 @@ enum PIN_INDEX {
     B,
     C
 };
-// using PIN_INDEX = enum pin_index;
 
 #define NUM_PINS 12
 constexpr int GPIO_PINS[NUM_PINS] = {8, 80, 78, 76, 79, 74, 73, 75, 71, 72, 77, 70};
 std::fstream GPIO_FILES[NUM_PINS];
-
-std::vector< std::vector<int> > sfu =  {
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 1, 1, 7, 1, 1, 1, 1, 1, 7, 1, 1, 7, 7, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 7, 7, 1, 1, 1, 1, 1, 1, 1, 7, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 1, 1, 1, 1, 1, 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 7, 7, 1, 1, 1, 1, 1, 7, 7, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 7, 7, 7, 1, 7, 7, 7, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 1, 1, 1, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 7, 7, 7, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 1, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 7, 7, 7, 1, 7, 7, 7, 1, 7, 1, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 7, 1, 1, 1, 7, 1, 1, 1, 7, 1, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 7, 7, 7, 1, 7, 7, 1, 1, 7, 1, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 7, 1, 7, 1, 1, 1, 7, 1, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 7, 7, 7, 1, 7, 1, 1, 1, 7, 7, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-};
 
 Display::Display(int num_chains,
                  int num_stack,
@@ -61,21 +44,18 @@ Display::Display(int num_chains,
                 ):  NUM_CHAIN{num_chains},
                     NUM_STACK{num_stack},
                     SERPENTINE{serpentine},
-                    NUM_PANELS{ NUM_CHAIN * NUM_STACK} {
-
-    exportPins();
-    setPinDirection();
-    setGPIOFiles();
-
-    framebuffer.reserve( PANEL_ROWS );
-
-    // start refresh thread
-    std::thread t1(&Display::ref, this);
-    t1.join();
-}
+                    NUM_PANELS{ num_chains * num_stack},
+                    framebuffer( PANEL_ROWS , std::vector<int>(PANEL_COLUMNS * NUM_PANELS, BLACK)),
+                    running{true},
+                    update{true},
+                    refresh_thread{&Display::refresh, this} { }
 
 Display::~Display(){
     // stop the thread
+    running = false;
+    refresh_thread.join();
+
+    // close the gpio files
     for(auto& file: GPIO_FILES){
         file.close();
     }
@@ -118,7 +98,7 @@ void Display::setGPIOFiles(){
     return;
 }
 
-void Display::setRow(int row) {
+void Display::setRow( int row ) {
     std::bitset<3> r = row;
     GPIO_FILES[ A ].seekp(0);
     GPIO_FILES[ A ] << r.test(0) << std::endl;// iiii & 0100
@@ -130,7 +110,7 @@ void Display::setRow(int row) {
     GPIO_FILES[ C ] << r.test(2) << std::endl;
 }
 
-void Display::setTop(int color) {
+void Display::setTop( int color ) {
     std::bitset<3> c = color;
 
     GPIO_FILES[ R1 ].seekp(0);
@@ -144,7 +124,7 @@ void Display::setTop(int color) {
 
 }
 
-void Display::setBtm(int color) {
+void Display::setBtm( int color ) {
     std::bitset<3> c = color;
 
     GPIO_FILES[ R2 ].seekp(0);
@@ -182,119 +162,89 @@ void Display::off() {
     GPIO_FILES[ OE ] << 1<< std::endl;
 }
 
-void Display::ref(){
-    // local copy of an array to push out to pins
+void Display::updateFrame( Frame frame ){
+    injestFrame( frame.frameBuffer );
+}
 
-    // try lock to update local from class array
-    while(true){
+void Display::updateFrame( std::vector<std::vector<int>>& frame ) {
+    assert( frame.size() == 32);
+    assert( frame.begin()->size() == 32);
+    injestFrame( frame );
+}
+
+void Display::injestFrame( const std::vector<std::vector<int>>& newFrame ) {
+    update_mutex.lock();
+
+    // get upper part the frame
+    std::vector<std::vector<int>> upper;
+    upper.assign( newFrame.begin(), newFrame.begin() + PANEL_ROWS);
+
+    // flip vertically
+    std::reverse(upper.begin(), upper.end());
+
+    // flip horizontally
+    std::for_each(
+        upper.begin(),
+        upper.end(),
+        [](auto& row){
+            std::reverse( row.begin(), row.end() );
+        }
+    );
+
+    framebuffer.clear();
+
+    framebuffer.reserve( PANEL_ROWS );
+    framebuffer.insert( framebuffer.begin(), upper.begin(), upper.end() );
+
+    auto lowerRow = newFrame.begin() + PANEL_ROWS;
+    for( auto& row : framebuffer ) {
+        row.insert( row.end(), lowerRow->begin(), lowerRow->end() );
+        ++lowerRow;
+    }
+
+    update = true;
+    update_mutex.unlock();
+}
+
+void Display::refresh(){
+    exportPins();
+    setPinDirection();
+    setGPIOFiles();
+
+    // local copy of an array to push out to pins
+    std::vector<std::vector<int>> local_buffer;
+
+    while(running){
+        // try lock to update local from class array
+        if( update && update_mutex.try_lock() ){
+            local_buffer.clear();
+            local_buffer.assign( framebuffer.begin(), framebuffer.end());
+            update = false;
+            update_mutex.unlock();
+        }
+        // std::cout << local_buffer.size() << "\t" << local_buffer.begin()->size() << std::endl;
+
         for( int i = 0; i < PANEL_ROWS/2; ++i ){ // cannot do foreach cuz looping only half rows
             // diable matrix
             off();
-
             // set row
             // a b c // TODO: using std::bitset instead of comparison
             setRow(i);
-
             // loop over column and send the data
                 for(int col = 0; col < 64; col++){
                     // upper, r1, g1, b1
-                    setTop( sfu[i][col]);
-
+                    setTop( local_buffer[i][col]);
                     // lower, r2, g2, b2
-                    setBtm( sfu[i+8][col]);
-
+                    setBtm( local_buffer[i+8][col]);
                     // toggle clock
                     clock();
                 } // for coloum
-
-
             // toggle latch
             lat();
-
             // enable display
             on();
 
             std::this_thread::sleep_for( std::chrono::nanoseconds( DELAY_IN_US ) );
         } // for row
     } // while
-}
-
-void Display::refresh(){
-    // local copy of an array to push out to pins
-
-    // try lock to update local from class array
-
-    while(true){
-        for( int i = 0; i < PANEL_ROWS/2; ++i ){ // cannot do foreach cuz looping only half rows
-            // diable matrix
-            GPIO_FILES[ OE ].seekp(0);
-            GPIO_FILES[ OE ] << 1 << std::endl;
-
-            // set row
-            // a b c // TODO: using std::bitset instead of comparison
-            int rr[3] = {
-                (i & 1),
-                        (i & 2),
-                        (i & 4)
-            };
-            GPIO_FILES[ A ].seekp(0);
-            GPIO_FILES[ A ] << rr[0];// iiii & 0100
-
-            GPIO_FILES[ B ].seekp(0);
-            GPIO_FILES[ B ] << (rr[1]>>1);
-
-            GPIO_FILES[ C ].seekp(0);
-            GPIO_FILES[ C ] << (rr[2]>>2);
-
-            // loop over column and send the data
-                for(int col = 0; col < 32; ++col){
-
-                    // upper, r1, g1, b1
-                    int frm = test2[i][col];
-                    int frm1 = test2[i+8][col];
-
-                    GPIO_FILES[ R1 ].seekp(0);
-                    GPIO_FILES[ R1 ] << (frm & 1);
-
-                    GPIO_FILES[ G1 ].seekp(0);
-                    GPIO_FILES[ G1 ] << ((frm & 2) >>1 );
-
-                    GPIO_FILES[ B1 ].seekp(0);
-                    GPIO_FILES[ B1 ] << ((frm & 4) >> 2);
-
-                    // lower, r2, g2, b2
-                    GPIO_FILES[ R2 ].seekp(0);
-                    GPIO_FILES[ R2 ] << (frm1 & 1);
-
-                    GPIO_FILES[ G2 ].seekp(0);
-                    GPIO_FILES[ G2 ] << ((frm1 & 2) >>1 );
-
-
-                    GPIO_FILES[ B2 ].seekp(0);
-                    GPIO_FILES[ B2 ] << ((frm1 & 4) >> 2);
-
-
-                    // toggle clock
-                    GPIO_FILES[ CLK ].seekp(0);
-                    GPIO_FILES[ CLK ] << 1;
-
-                    GPIO_FILES[ CLK ].seekp(0);
-                    GPIO_FILES[ CLK ] << 0;
-                }
-
-
-            // toggle latch
-            GPIO_FILES[ LAT ].seekp(0);
-            GPIO_FILES[ LAT ] << 1;
-
-            GPIO_FILES[ LAT ].seekp(0);
-            GPIO_FILES[ LAT ] << 0;
-
-            // enable display
-            GPIO_FILES[ OE ].seekp(0);
-            GPIO_FILES[ OE ] << 0;
-
-            std::this_thread::sleep_for( std::chrono::nanoseconds(5) );
-        }
-    }
 }
